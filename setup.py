3b39264be275b43e85c1f6c5dd1ff1e76f803619
AmitRM <32505616+amitrm@users.noreply.github.com>
2020-12-25 18:04:46 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding = 'utf-8') as fh:
 
 setuptools.setup(
     name = 'ajaira', 
-    version = '0.0.1',
+    version = '0.0.2',
     author = 'Amit Ranjan Mondal',
     author_email = 'amit.armondal@gmail.com',
     description = 'This packages does nothing.',
