2625a82a27de3c6f144ec7ccafc2b19e18d71224
amitrm <32505616+amitrm@users.noreply.github.com>
2021-01-31 20:36:56 -0600

    Added comment on time.py

--- ajaira/time.py
+++ ajaira/time.py
@@ -1,4 +1,8 @@
 from datetime import datetime
 
 def time_now():
+    '''
+    Returns the current timestamp for printing or logging.
+    '''
+
     return datetime.now().strftime("%m-%d-%Y %H:%M:%S")
