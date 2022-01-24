240b3d05d3c000e0a2b9f79a64cff296738d608e
amitrm <amit.armondal@gmail.com>
2022-01-24 16:08:16 -0600

    Added function call.

--- ajaira/jinis.py
+++ ajaira/jinis.py
@@ -1,8 +1,16 @@
+import geopandas as gpd
+print(gpd.__version__)
+
+
 def tell_me_about_it():
     
     message = """
     Thanks for installing this ajaira package.
-    Eat Dead!
+    This is a test file.
     """
 
     print(message)
+
+
+if __name__ == '__main__':
+    tell_me_about_it()
